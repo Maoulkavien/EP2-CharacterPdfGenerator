466486adc73c754e8e4f5ea88b61a1a3241ab83e
Maoulkavien <maoulkavien.github@smis.fr>
2020-01-20 16:45:52 +0100

    added clear PDF for ego generation

--- ego.php
+++ ego.php
@@ -3,7 +3,7 @@ require('lib/libego.php');
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Create an instance of the class:
-$pdf = newPDF('Maoulkavien-EP2_Ego.pdf');
+$pdf = newPDF('Maoulkavien-EP2_Ego-blank.pdf');
 
 $char = loadCharacter($_POST['data']);
 $params = loadParameters($_POST);
